9cf493249b3bf9f0493e50c9361edeb4c1096f40
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2021-03-29 02:21:59 +0100

    feat/add middleware for message endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { modifyMessage } from '../middleware';
 import { indexPage, messagesPage, addMessage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
--- test/messages.test.js
+++ test/messages.test.js
@@ -29,7 +29,7 @@ it('posts Message', done => {
       res.body.messages.forEach(m => {
         expect(m).to.have.property('id');
         expect(m).to.have.property('name', data.name);
-        expect(m).to.have.property('message', data.message);
+        expect(m).to.have.property('message', `SAYS: ${data.message}`);
       });
       done();
     });
