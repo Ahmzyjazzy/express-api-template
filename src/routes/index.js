e71142ac78613e72d939ef6c6800e82cfbc100ac
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2021-03-29 02:33:36 +0100

    feat/add async middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction } from '../middleware';
 import { indexPage, messagesPage, addMessage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
